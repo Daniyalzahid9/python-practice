0ae15610dc6a7611ca26780a0c478bfd1a3ca3b2
Muhammad Daniyal Hussain <157269036+Daniyalzahid9@users.noreply.github.com>
2026-03-23 01:57:53 +0500

    Create dict-practice.py

---
+++ week-1/dict-practice.py
@@ -0,0 +1,25 @@
+dict = {
+    "name":"daniyal",
+    "age": 20,
+    "Education_Institute":"Fast Nuces",
+    "Field":"Fintech",
+    "subjects":["Eng","Fianancial Accounting ","Programming For Business","Management"],
+    
+     "Marks":{
+        "Eng":70,
+        "Financial Accounting":86,
+        "Programming For Business":69,
+        "Management":85,
+                         }
+}
+
+print(f"{dict}\n")
+print(type(dict))
+print(dict["name"])
+print(dict["age"])
+print(dict["Education_Institute"])
+print(dict["Field"])
+print(dict["subjects"])
+print(dict["Marks"]["Management"])
+
+                  
